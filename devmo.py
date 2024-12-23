3f0df99970f28911f878c997a7ee6411a24c8797
Rahul Wagh <rahulwagh28032003@gmail.com>
2024-12-23 16:58:12 +0530

    changes12

--- devmo.py
+++ devmo.py
@@ -1,9 +1,18 @@
-def function():
+"""
+This module provides a demonstration of a simple function.
+"""
 
+def function():
+    """
+    Calculates the sum of two numbers and returns the result.
+
+    Returns:
+        int: The sum of 1 and 3.
+    """
     a = 1
     b = 3
     c = a + b
     return c
 
-
-function()
+result = function()
+print(result)
